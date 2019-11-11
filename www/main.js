c4083a9597ba77edd2f1e7697e5dee0428fba771
caffeineDream <sweetsleepomine@gmail.com>
2019-11-11 04:24:05 +0300

    misc

--- www/main.js
+++ www/main.js
@@ -1,66 +1,72 @@
-// Generate 2D-array, populate it with random ones and zeroes.
-function generateRandomArray(N) {
-    // Create empty 2D-array 
-    var initMatrix = new Array(N);
-    for (let i = 0; i < initMatrix.length; i++) {
-        initMatrix[i] = new Array(N);
-    };
-    // Populating array with random ones and zeroes
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N ; j++) {
-            initMatrix[i][j] = Math.round(Math.random());
-        };
-    };
-    // Thin out ones in array by half
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N ; j++) {
-          if (initMatrix[i][j] = 1) {
-              console.log('hi');
-          }   
-        };
-    };
 
-    return initMatrix;
+// Как завернуть Svg в функцию, но потом норально передать его в visualize?
+var originMatrix = generateRandomArray(8);
+
+var playground = {
+    length: originMatrix.length,
+    cellSize: 50
 };
 
-
-
-
-
-var matrix = generateRandomArray(5);
-console.log(matrix);
-
-var cols = matrix.length;
-var rows = matrix[0].length;
-var cellSize = 50;
-
-var svg = d3.select("#playground")
+var svg = d3.select("#playgroundContainer")
                 .append("svg")
-                .attr("width", cols * cellSize)
-                .attr("height", rows * cellSize);
+                .attr("width", playground.length * playground.cellSize)
+                .attr("height", playground.length * playground.cellSize);
     svg.append("rect")
     .attr("width", "100%")
     .attr("height", "100%")
     .attr("fill", "pink");
 
+visualize(originMatrix);
+
+//-------------------------------------------------------------
+// Generate 2D-array, populate it with dead and living cells
+function generateRandomArray(N) {
+    // Create empty 2D-array 
+    var randomArray = new Array(N);
+    for (let i = 0; i < randomArray.length; i++) {
+        randomArray[i] = new Array(N);
+    };
+    // Populating array with dead and living cells ~ 50/50
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N ; j++) {
+            randomArray[i][j] = Math.round(Math.random());
+        };
+    };
+    // Thin out living cells. Place counter increment inside if statement
+    // to increase amout of living cells.
+    var counter = 0;
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N ; j++) {
+          if (randomArray[i][j] == 1 && counter % 2 == 0) {
+             randomArray[i][j] = 0;
+          };
+          counter += 1;
+        };
+    };  
+    return randomArray;
+};
+
+// Takes array and fills svg with it
+function visualize(matrix) {
     svg.append("g")
-                .selectAll("g")                 
-                .data(matrix)
-                .enter()
-                .append("g")
-                .attr('transform', function (d, i) {
-                    return 'translate(' + i * cellSize + ')'
-                })
-                .selectAll("text")
-                .data( function(d) { return d; } )
-                .enter()
-                .append("text")
-                .text( function(d,i,j) { return d; } )
-                .attr("x", function(d, i, j) {
-                    return 30;
-                })
-                .attr('y', function(d, i) {
-                    return i * cellSize + 30;
-                })
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "20px");
+        .selectAll("g")                 
+        .data(matrix)
+        .enter()
+        .append("g")
+        .attr('transform', function (d, i) {
+            return 'translate(' + i * playground.cellSize + ')'
+        })
+        .selectAll("text")
+        .data( function(d) { return d; } )
+        .enter()
+        .append("text")
+        .text( function(d,i,j) { return d; } )
+        .attr("x", function(d, i, j) {
+            return 30;
+        })
+        .attr('y', function(d, i) {
+            return i * playground.cellSize + 30;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "20px");
+};
